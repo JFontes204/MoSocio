b6b52046931d62f88ce6e261fb33e04341bdfd56
RaMadaSilva <raul01mateia@gmail.com>
2023-09-22 11:04:04 +0100

    pequenas alterações no Modelo

--- MoSocioAPI.Model/Role.cs
+++ MoSocioAPI.Model/Role.cs
@@ -8,6 +8,8 @@ namespace InvoicingPlan.Model
     {
         [Key]
         public int Id { get; set; }
+        [Required]
+        [MaxLength(40)]
         public string Name { get; set; }
     }
 }
---
+++ MoSocioAPI.Model/UserRole.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace InvoicingPlan.Model
+{
+    [Table(nameof(UserRole))]
+    public class UserRole
+    {
+        public int UserId { get; set; }
+        public int RoleId { get; set; }
+    }
+}
