ad462feb4734bddefbd34575c9ae8bd8ee7cf898
RaMadaSilva <raul01mateia@gmail.com>
2023-09-24 16:33:04 +0100

    acriando o serviço para gerar o token

---
+++ MoSocioAPI.Shared/ConfigurateCredential.cs
@@ -0,0 +1,7 @@
+﻿namespace MoSocioAPI.Shared
+{
+    public static class ConfigurateCredential
+    {
+        public static string secretKey = "166aeeb876b6a0411af182dd2c9d4047af406b2ae06906014cb4bf20a57ceb6e"; 
+    }
+}
